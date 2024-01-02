4eb63d51ece8ce3df8e7986332c1cabe1edeacb6
ezhillragesh <rag3shx@gmail.com>
2024-01-02 19:16:15 +0530

    Added Docs

--- comps/Collection.tsx
+++ comps/Collection.tsx
@@ -1,104 +1,55 @@
-"use client"
-import React, { useState, useEffect, useCallback } from 'react';
+'use client'
+import React, { useState } from 'react';
+import useSWR from 'swr';
 import Tile from './Tile.tsx';
 import organizations from '@/public/organizations.json';
 
-
 const Collection: React.FC = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const [errorMessageDisplayed, setErrorMessageDisplayed] = useState(false);
 
-  useEffect(() => {
-    // Load data from cache if available
-    const cachedData = localStorage.getItem('issuesData');
-    if (cachedData) {
-      setData(JSON.parse(cachedData));
-    } else {
-      fetchData();
+  const tiles = organizations.map((org) => {
+    const apiUrl = `https://api.github.com/search/issues?q=is:open+is:issue+org:${org.org}+label:%22good+first+issue%22`;
+    const token = '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';
+    const { data, error } = useSWR(apiUrl, (url) => fetcher(url, { headers: { Authorization: `Bearer ${token}` } }));
+
+
+    
+
+    if (error) return <div key={org.org}>Failed to load for {org.org}</div>;
+
+    // Ensure data is available and has items
+    if (!data || !data.items) {
+      if (!errorMessageDisplayed) {
+        setErrorMessageDisplayed(true);
+        return (
+          <p key="error-message" className='text-red-400'>
+            Wait for a few seconds and refresh...
+          </p>
+        );
+      } else {
+        return null; // Return null if the error message has already been displayed
+      }
     }
-  }, []);
 
-  const fetchData = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-
-    try {
-      // Include your personal access token
-      const token = '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'; // Replace with your actual token
-
-      const promises = organizations.map(async (org) => {
-        const apiUrl = `https://api.github.com/search/issues?q=is:open+is:issue+org:${org.org}+label:"${org.label}"`;
-        const response = await fetch(apiUrl, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (response.status === 403 || response.status === 429) {
-          throw new Error('Rate limit exceeded. Please try again later.');
-        }
-
-        const result = await response.json();
-        return { org: org.org, data: result };
-      });
-
-      const results = await Promise.all(promises);
-      const combinedData = results.reduce((acc, { org, data }) => {
-        acc[org] = data;
-        return acc;
-      }, {});
-
-      setData(combinedData);
-      localStorage.setItem('issuesData', JSON.stringify(combinedData)); // Cache data
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
-
-  if (error) {
     return (
-      <div>
-        <p className='text-red-600'>{error}</p>
+      <div key={org.org} className='lg:grid grid-cols-2 my-0 justify-center items-center gap-4 '>
+        {data.items.map((item) => (
+          <a href={item.html_url} key={item.id} target='_blank'>
+            <Tile
+              title={item.title}
+              body={item.body}
+              organization={org.org}
+              language={org.language}
+              date={item.created_at}
+            />
+          </a>
+        ))}
       </div>
     );
-  }
+  });
 
-  if (!data) {
-    return (
-      <div>
-        <p className='text-red-600'>
-          {isLoading ? 'Loading data...' : 'If you are seeing this, refresh and wait for a few seconds ...'}
-        </p>
-      </div>
-    );
-  }
-
-  return (
-    <main className='grid my-0 mx-auto justify-center items-center gap-4 '>
-      {organizations.map((org) => (
-        <div key={org.org} className='lg:grid grid-cols-2 my-0 justify-center items-center gap-4 '>
-          {data[org.org] && data[org.org].items ? (
-            data[org.org].items.map((item) => (
-              <a href={item.html_url} key={item.id} target='_blank' rel='noopener noreferrer'>
-                <Tile
-                  title={item.title}
-                  body={item.body}
-                  organization={org.org}
-                  language={org.language}
-                  date={item.created_at}
-                />
-              </a>
-            ))
-          ) : (
-            <p>Loading...</p>
-          )}
-        </div>
-      ))}
-    </main>
-  );
+  return <main className='grid my-0 mx-auto justify-center items-center gap-4 '>{tiles}</main>;
 };
 
 export default Collection;
