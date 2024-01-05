d80fa5f6f4a3280c29db05cee97819a61d28d61a
ezhillragesh <rag3shx@gmail.com>
2024-01-05 22:10:18 +0530

    navbar mobile ui

--- comps/navbar.tsx
+++ comps/navbar.tsx
@@ -1,6 +1,17 @@
 "use client"
 import { NavigationMenuLink, NavigationMenuItem, NavigationMenuList, NavigationMenu } from "@/components/ui/navigation-menu"
 import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,} from "@/components/ui/sheet.tsx"
 export default function Navbar() {
   return (
     <div className="my-4 mx-auto">
@@ -40,6 +51,7 @@ export default function Navbar() {
             Issues
           </NavigationMenuLink>
         </NavigationMenuItem>
+
         <NavigationMenuItem>
           <NavigationMenuLink
             className="group sm:block hidden h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
@@ -48,10 +60,38 @@ export default function Navbar() {
             Join Us
           </NavigationMenuLink>
         </NavigationMenuItem>
+
+
+
+
+{/* mobile navbar */}
+        <NavigationMenuItem>
+          <NavigationMenuLink className="sm:hidden">
+          <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Menu</Button>
+      </SheetTrigger>
+      <SheetContent className="dark:text-white">
+        <SheetHeader>
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col gap-5 w-[100%] items-center ">
+         <a href="about"><Button className="w-24">About</Button></a>
+         <a href="programs"><Button className="w-24">Programs</Button></a>
+         <a href="issues"><Button className="w-24">Issues</Button></a>
+         <a href="join"><Button className="w-24">Join us</Button></a>
+        </div>
+      </SheetContent>
+    </Sheet>
+            
+            
+             
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+
       </NavigationMenuList>
     </NavigationMenu>
     </div>
   )
 }
-
-
