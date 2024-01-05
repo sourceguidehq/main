aff13c90d83925f79145c0c4c8783dfbbedbc72c
ezhillragesh <rag3shx@gmail.com>
2024-01-05 19:11:46 +0530

    minor fix

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ module.exports = {
     eslint: {
       ignoreDuringBuilds: true,
     },
-    env: {
-        NEXT_PUBLIC_AUTH_TOKEN: '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',
-      },
+    
       typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
