58c76a387b7f8b2dad4b28cf2d7de8175eef72e6
adhitya nantish <adhitya2k3@gmail.com>
2024-01-06 10:46:39 +0530

    changed logo colour

--- comps/navbar.tsx
+++ comps/navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink
-            className="group inline-flex mt-1 h-8 w-max items-center justify-center   px-4 py-2 text-sm font-medium transition-colors    focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+            className="group rounded-md inline-flex  mt-1 h-8 w-max items-center justify-center   px-4 py-2 text-sm font-medium transition-colors    focus:outline-none disabled:pointer-events-none disabled:opacity-50"
             href="/"
           >
         
