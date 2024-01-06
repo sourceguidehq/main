928452a2c032a859cd7679daa9fd0fe63e05bfd9
adhitya nantish <adhitya2k3@gmail.com>
2024-01-06 10:06:56 +0530

    faq light mode

--- comps/Faq.tsx
+++ comps/Faq.tsx
@@ -19,10 +19,10 @@ export default function Faq() {
       <Accordion type="single" collapsible className="w-full text-left">
         {/* Question 1 */}
         <AccordionItem value="item-1">
-          <AccordionTrigger className="font-bold text-white text-xl">
+          <AccordionTrigger className="font-bold dark:text-white text-xl">
             How can I contribute to SourceGuide?
           </AccordionTrigger>
-          <AccordionContent className="text-white">
+          <AccordionContent className="dark:text-white">
             Contributing to SourceGuide is easy! Simply visit our{' '}
             <a
               href="https://github.com/SourceGuideHQ"
@@ -38,20 +38,20 @@ export default function Faq() {
         
         {/* Question 2 */}
         <AccordionItem value="item-2">
-          <AccordionTrigger className="font-bold text-white text-xl">
+          <AccordionTrigger className="font-bold dark:dark:text-white text-xl ">
             Is SourceGuide only for experienced developers?
           </AccordionTrigger>
-          <AccordionContent className="text-white">
+          <AccordionContent className="dark:text-white">
             No, SourceGuide is inclusive for everyone, regardless of experience level. We curate projects with beginner-friendly issues, making it an ideal platform for those new to open source. Whether you're a seasoned developer or just starting, there's a place for you to learn, contribute, and grow with SourceGuide.
           </AccordionContent>
         </AccordionItem>
         
         {/* Question 3 */}
         <AccordionItem value="item-3">
-          <AccordionTrigger className="font-bold text-white text-xl">
+          <AccordionTrigger className="font-bold dark:text-white text-xl">
           Who started this initiative?
           </AccordionTrigger>
-          <AccordionContent className="text-white">
+          <AccordionContent className="dark:text-white">
           The SourceGuide initiative was started by{' '}
             <a
               href="https://github.com/ezhillragesh"
@@ -77,20 +77,20 @@ export default function Faq() {
         
         {/* Question 4 */}
         <AccordionItem value="item-4">
-          <AccordionTrigger className="font-bold text-white text-xl">
+          <AccordionTrigger className="font-bold dark:text-white text-xl">
           Are all GSoC organizations included here?
           </AccordionTrigger>
-          <AccordionContent className="text-white">
+          <AccordionContent className="dark:text-white">
           We include organizations that have participated in GSoC over the last 5 years consistently.
           </AccordionContent>
         </AccordionItem>
         
         {/* Question 5 */}
         <AccordionItem value="item-5">
-          <AccordionTrigger className="font-bold text-white text-xl">
+          <AccordionTrigger className="font-bold dark:text-white text-xl">
           How does it find beginner-friendly issues?
           </AccordionTrigger>
-          <AccordionContent className="text-white">
+          <AccordionContent className="dark:text-white">
           We identify beginner-friendly issues by searching for those tagged as "good-first-issue" or "beginner-friendly" and display them for contributors.
           </AccordionContent>
         </AccordionItem>
