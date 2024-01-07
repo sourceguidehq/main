3460ca12e15717bfaf932c1234d71808191b9fc8
adhitya nantish <adhitya2k3@gmail.com>
2024-01-07 16:12:19 +0530

    fixed alignment for filter

--- app/issues/[org]/page.tsx
+++ app/issues/[org]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import Tile from '@/comps/Tile';
+import Link from 'next/link';
 
 
 type Props = {
@@ -15,10 +16,13 @@ export default function page({ params }: { params: { org: string } }) {
 
 
     
+
   return (
+
+    <div className='w-[100%] flex items-center justify-center'>
     <div key={data.org} className='lg:grid grid-cols-2 my-0 justify-center items-center gap-4 '>
     {data.items.map((item) => (
-      <a href={item.html_url} key={item.id} target='_blank' rel='noopener noreferrer'>
+      <Link href={item.html_url} key={item.id} target='_blank' rel='noopener noreferrer'>
         <Tile
           title={item.title}
           body={item.body}
@@ -26,8 +30,9 @@ export default function page({ params }: { params: { org: string } }) {
           language={data.language}
           date={item.created_at}
         />
-      </a>
+      </Link>
     ))}
+  </div>
   </div>
 
   )
